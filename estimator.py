b6094dc2d471948431ab656e320f95d1dac943e6
benckx <benoit.vleminckx@restore.eu>
2020-09-02 16:05:36 +0200

    git hours estimator

--- estimator.py
+++ estimator.py
@@ -1,7 +1,36 @@
 import sys
 
+one_hour_in_sec = 60 * 60
+threshold = 2 * one_hour_in_sec
+
+timestamps = []
+
 for line in sys.stdin:
     timestamp = int(line.replace('\n', ''))
-    print(timestamp)
+    timestamps.append(timestamp)
+
+total_time_sec = 0
+
+timestamps.sort()
+previous_timestamp = timestamps[0]
+current_session = []
+nbr_of_sessions = 0
+
+for x in range(1, len(timestamps)):
+    delta = timestamps[x] - previous_timestamp
+    previous_timestamp = timestamps[x]
+
+    if delta > threshold:
+        if len(current_session) >= 2:
+            start = current_session[0]
+            end = current_session[len(current_session) - 1]
+            duration = end - start
+            total_time_sec += duration
+            current_session = []
+        nbr_of_sessions += 1
+    else:
+        current_session.append(timestamps[x])
+
+print(total_time_sec / one_hour_in_sec)
 
 sys.stdout.flush()
